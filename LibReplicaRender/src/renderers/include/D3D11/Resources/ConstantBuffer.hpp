e601199046e32b4d6048f97678acce55dfea2c59
Zach Hembree <zhembree654@gmail.com>
2025-04-05 18:34:01 -0400

    Fix constant buffer sizing issue
    Caused by obsolete templated constructor

--- LibReplicaRender/src/renderers/include/D3D11/Resources/ConstantBuffer.hpp
+++ LibReplicaRender/src/renderers/include/D3D11/Resources/ConstantBuffer.hpp
@@ -6,11 +6,6 @@ namespace Replica::D3D11
 	class ConstantBuffer : public BufferBase
 	{
 	public:
-		template<typename T>
-		ConstantBuffer(Device& device, const T& data) :
-			ConstantBuffer(device, sizeof(T), &data)
-		{ }
-
 		ConstantBuffer();
 
 		ConstantBuffer(Device& device, size_t size, const void* data = nullptr);
