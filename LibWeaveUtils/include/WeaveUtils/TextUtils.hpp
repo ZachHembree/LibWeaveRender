1212ecc6e5f4b327dd0070e9779c4a5835cccd61
Zach Hembree <zhembree654@gmail.com>
2025-04-26 15:48:44 -0400

    Add vformat utils

--- LibWeaveUtils/include/WeaveUtils/TextUtils.hpp
+++ LibWeaveUtils/include/WeaveUtils/TextUtils.hpp
@@ -1,5 +1,7 @@
+#pragma once
 #include <string>
 #include <string_view>
+#include <format>
 
 namespace Weave
 {
@@ -37,4 +39,23 @@ namespace Weave
 	/// Returns a copy of a given narrow UTF-8 string as a UTF-16LE string.
 	/// </summary>
 	void GetWideString_UTF8_TO_UTF16LE(string_view src, wstring& dst);
+
+	/// <summary>
+	/// Runtime std::format wrapper.
+	/// </summary>
+	template<typename... FmtArgs>
+	inline void VFormat(string_view fmt, FmtArgs&&... args)
+	{
+		std::vformat(fmt, std::make_format_args(args...));
+	}
+
+	/// <summary>
+	/// Runtime std::format wrapper. Writes formatted text directly to preexisting string using back_insert 
+	/// iterator.
+	/// </summary>
+	template<typename... FmtArgs>
+	inline void VFormatTo(string& dst, string_view fmt, FmtArgs&&... args)
+	{
+		std::vformat_to(std::move(std::back_inserter(dst)), fmt, std::make_format_args(args...));
+	}
 }
