eb3a0e06d249172a1f4eee597905aa94ce79ac99
Zach Hembree <zhembree654@gmail.com>
2025-04-27 14:24:18 -0400

    Add utilities for finding display modes for DisplayOutput

--- LibWeaveRender/src/renderers/include/D3D11/Resources/DisplayOutput.hpp
+++ LibWeaveRender/src/renderers/include/D3D11/Resources/DisplayOutput.hpp
@@ -1,10 +1,14 @@
 #pragma once
+#include "WeaveUtils/MinWindow.hpp"
 #include "../InternalD3D11.hpp"
 #include "DeviceChild.hpp"
 
 namespace Weave::D3D11
 {
-	struct DisplayModes
+	/// <summary>
+	/// Defines a resolution and associated refresh rates supported by a given DisplayOutput
+	/// </summary>
+	struct DisplayMode
 	{
 		/// <summary>
 		/// Resolution supported by the display mode. x == width. y == height.
@@ -17,9 +21,9 @@ namespace Weave::D3D11
 		UniqueVector<uivec2> refreshRates;
 
 		/// <summary>
-		/// Supported format
+		/// Internal unique identifier
 		/// </summary>
-		Formats format;
+		uint id;
 
 		/// <summary>
 		/// Returns the refresh rate in cycles per second (Hz)
@@ -29,8 +33,17 @@ namespace Weave::D3D11
 			const uivec2 rat = refreshRates[index];
 			return (double)rat.x / (double)rat.y;
 		}
+
+		/// <summary>
+		/// Internal indices used to uniquely identify a display mode configuration for a given 
+		/// DisplayOutput
+		/// </summary>
+		uivec2 GetID(uint index) const { return uivec2(id, index); }
 	};
 
+	/// <summary>
+	/// Provides an interface to a DXGI display output
+	/// </summary>
 	class DisplayOutput : public DeviceChild
 	{
 	public:
@@ -60,6 +73,17 @@ namespace Weave::D3D11
 		HMONITOR GetHandle() const;
 
 		/// <summary>
+		/// Returns default monitor config used by the desktop on startup
+		/// </summary>
+		const WinMonConfig& GetWinDefaults() const;
+
+		/// <summary>
+		/// Returns mode ID that most closely aligns with the desktop settings on startup.
+		/// Requires a format to be specified first.
+		/// </summary>
+		uivec2 GetDefaultMode() const { return defaultMode; }
+
+		/// <summary>
 		/// Initialzies or changes the display output to the given format and updates supported modes.
 		/// Unsupported formats will result in an empty mode list.
 		/// </summary>
@@ -71,15 +95,24 @@ namespace Weave::D3D11
 		Formats GetFormat() const;
 
 		/// <summary>
+		/// Returns the DisplayMode + frequency identifier most closely matching the given resolution 
+		/// and refresh rate. Mode ID == (uint)-1 on failure.
+		/// </summary>
+		uivec2 GetClosestMatch(uivec2 res, uivec2 refresh = uivec2(0)) const;
+
+		/// <summary>
 		/// Returns an array of supported resolutions and framerates
 		/// </summary>
-		const IDynamicArray<DisplayModes>& GetModes() const;
+		const IDynamicArray<DisplayMode>& GetModes() const;
 
 	private:
 		string name;
 		HMONITOR handle;
+		WinMonConfig defaults;
+
 		Formats format;
 		ComPtr<IDXGIOutput1> pDisplay;
-		UniqueVector<DisplayModes> dispModes;
+		UniqueVector<DisplayMode> dispModes;
+		uivec2 defaultMode;
 	};
 }
--- LibWeaveRender/src/renderers/src/D3D11/Resources/DisplayOutput.cpp
+++ LibWeaveRender/src/renderers/src/D3D11/Resources/DisplayOutput.cpp
@@ -1,11 +1,12 @@
 #include "pch.hpp"
+#include "WeaveUtils/MinWindow.hpp"
 #include "D3D11/Resources/DisplayOutput.hpp"
 #include "D3D11/Device.hpp"
 
 using namespace Weave;
 using namespace Weave::D3D11;
 
-DisplayOutput::DisplayOutput() : handle(nullptr), format(Formats::UNKNOWN) {}
+DisplayOutput::DisplayOutput() : handle(nullptr), format(Formats::UNKNOWN), defaults({}) {}
 
 DisplayOutput::~DisplayOutput() {}
 
@@ -16,11 +17,15 @@ DisplayOutput& DisplayOutput::operator=(DisplayOutput&&) noexcept = default;
 DisplayOutput::DisplayOutput(Device& dev, ComPtr<IDXGIOutput1>&& pOutput, string&& name) :
 	DeviceChild(dev),
 	pDisplay(std::move(pOutput)),
-	name(std::move(name))
+	name(std::move(name)),
+	format(Formats::UNKNOWN),
+	defaultMode(-1, -1)
 { 
 	DXGI_OUTPUT_DESC desc = {};
 	pDisplay->GetDesc(&desc);
 	handle = desc.Monitor;
+
+	defaults = MinWindow::GetMonitorConfig(handle);
 }
 
 IDXGIOutput1* DisplayOutput::Get() const { return pDisplay.Get(); }
@@ -38,7 +43,7 @@ void DisplayOutput::SetFormat(Formats format)
 
 	D3D_ASSERT_HR(pDisplay->GetDisplayModeList1((DXGI_FORMAT)format, 0, &modeCount, modes.GetData()));
 
-	DisplayModes* pLastMode = nullptr;
+	DisplayMode* pLastMode = nullptr;
 
 	for (const DXGI_MODE_DESC1& mode : modes)
 	{
@@ -51,9 +56,12 @@ void DisplayOutput::SetFormat(Formats format)
 
 			if (pLastMode == nullptr || pLastMode->resolution != newRes)
 			{
-				pLastMode = &dispModes.EmplaceBack(DisplayModes{});
-				pLastMode->resolution = newRes;
-				pLastMode->format = (Formats)mode.Format;
+				const uint id = (uint)dispModes.GetLength();
+				pLastMode = &dispModes.EmplaceBack(DisplayMode
+				{
+					.resolution = newRes,
+					.id = id
+				});
 			}
 
 			const uivec2 newRefresh = uivec2(mode.RefreshRate.Numerator, mode.RefreshRate.Denominator);
@@ -62,13 +70,66 @@ void DisplayOutput::SetFormat(Formats format)
 				pLastMode->refreshRates.Add(newRefresh);
 		}
 	}
+
+	defaultMode = GetClosestMatch(defaults.res, uivec2(defaults.refreshHz, 1u));
 }
 
 Formats DisplayOutput::GetFormat() const { return format; }
+
+uivec2 DisplayOutput::GetClosestMatch(uivec2 res, uivec2 refresh) const
+{
+	uint closestRes = (uint)-1;
+	uint resErr = (uint)-1;
+
+	// Find closest matching resolution by minimizing area error
+	for (uint i = 0; i < (uint)dispModes.GetLength(); i++)
+	{
+		const ivec2 resDelta = ivec2(dispModes[i].resolution) - ivec2(res);;
+		const uint newErr = (uint)std::abs(resDelta.x * resDelta.y);
+
+		if (newErr < resErr)
+		{
+			closestRes = i;
+			resErr = newErr;
+		}
+	}
+
+	const IDynamicArray<uivec2>& refreshRates = dispModes[closestRes].refreshRates;
+	const uint defaultRefresh = (uint)refreshRates.GetLength() - 1;
+
+	if (closestRes == (uint)-1) // Match failed
+		return uivec2((uint)-1);
+	else if (refresh.y == 0) // Use default (highest) refresh rate
+		return uivec2(closestRes, defaultRefresh);
+
+	// Find closest refresh by minimizing frequency error in Hz
+	uint closestRefresh = (uint)-1;
+	const double targetRefreshHz = refresh.x / refresh.y;
+	double lastRefreshErr = std::numeric_limits<double>::max();
+
+	for (uint i = 0; i < (uint)refreshRates.GetLength(); i++)
+	{
+		const double modeRefreshHz = refreshRates[i].x / refreshRates[i].y;
+		double newErr = std::abs(modeRefreshHz - targetRefreshHz);
+
+		if (newErr < lastRefreshErr)
+		{
+			closestRefresh = i;
+			lastRefreshErr = newErr;
+		}
+	}
+
+	if (closestRefresh != (uint)-1) // Match found
+		return uivec2(closestRes, closestRefresh);
+	else // Match failed - use default (highest) refresh rate
+		return uivec2(closestRes, defaultRefresh);
+}
 
 string_view DisplayOutput::GetName() const { return name; }
 
 HMONITOR DisplayOutput::GetHandle() const { return handle; }
 
-const IDynamicArray<DisplayModes>& DisplayOutput::GetModes() const { return dispModes; }
+const WinMonConfig& DisplayOutput::GetWinDefaults() const { return defaults; }
+
+const IDynamicArray<DisplayMode>& DisplayOutput::GetModes() const { return dispModes; }
 
